a9b632e9c307efc37a6d9ab7fe1a2269f9eb8bf8
erkelost <1256029807@qq.com>
2024-03-19 16:05:23 +0800

    chore: update farm config

--- farm.config.mjs
+++ farm.config.mjs
@@ -1,5 +1,20 @@
 import { defineConfig } from "@farmfe/core";
 
 export default defineConfig({
+  compilation: {
+    presetEnv: false,
+    // minify: {
+    //   mode: "minify-resource-pot"
+    // },
+    persistentCache: false,
+    // partialBundling: {
+    //   enforceResources: [
+    //     {
+    //       name: "index.js",
+    //       test: [".+"],
+    //     },
+    //   ],
+    // },
+  },
   plugins: ["@farmfe/plugin-react"],
 });
